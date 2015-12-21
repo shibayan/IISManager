43e17aa6a7624d7aa0edca6a0e81d3f310a0268d
shibayan <shiba-yan@outlook.com>
2015-12-21 13:46:24 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+IIS Manager Extension
+================
+
+## License
+
+[Apache License 2.0](https://github.com/shibayan/IISManager/blob/master/LICENSE)
---
+++ src/IISManager/Models/AppHost.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+using FatAntelope;
+using FatAntelope.Writers;
+
+namespace IISManager.Models
+{
+    public class AppHost
+    {
+        public static string GetCurrentSettings()
+        {
+            var appPoolConfig = Environment.GetEnvironmentVariable("APP_POOL_CONFIG");
+
+            if (appPoolConfig == null)
+            {
+                return "";
+            }
+
+            return File.ReadAllText(appPoolConfig);
+        }
+
+        public static void SaveAppHostXdt(string str)
+        {
+            var appHostXdt = Environment.ExpandEnvironmentVariables(@"%HOME%\site\applicationHost.xdt");
+
+            File.WriteAllText(appHostXdt, str);
+        }
+
+        public static string GenerateXdt(string baseConfig, string newConfig)
+        {
+            XDiff.Diff(null, null);
+
+            var writer = new XdtDiffWriter();
+
+            var patch = writer.GetDiff(null);
+            
+            var textWriter = new StringWriter();
+
+            patch.Save(textWriter);
+
+            return textWriter.ToString();
+        }
+    }
+}
